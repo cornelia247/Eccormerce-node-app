d836c4042cb05cac95e856f280eae04105a11cb5
cornelia247 <ekpenyongcornelia2@gmail.com>
2021-08-26 21:44:07 +0100

    updated user model

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
-  }).then( result => {
+    description: description,
+    userId: req.user.id
+  })
+  .then( result => {
     console.log('Created Product')
     res.redirect('/admin/products')
   })
@@ -31,8 +33,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product => {
+  req.user
+  .getProducts({ where: { id: prodId}})
+  .then(products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -70,7 +74,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  .getProducts()
   .then( products => {
     res.render('admin/products', {
       prods: products,
