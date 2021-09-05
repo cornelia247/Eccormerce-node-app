897abc8df08b63e0abc57697cd0aa3ee5cadb488
cornelia247 <ekpenyongcornelia2@gmail.com>
2021-09-05 23:34:47 +0100

    sequelise roundoff

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product')
 const User = require('./models/user')
 const Cart = require('./models/cart')
 const CartItem = require('./models/cart-item')
+const Order = require('./models/order')
+const OrderItem = require('./models/order-item')
 
 
 
@@ -45,30 +47,31 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
+
 
 sequelize
-//.sync( { force: true})
-.sync()
-.then( result =>  {
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
     return User.findByPk(1);
-    
-
-})
-.then( user => {
+    // console.log(result);
+  })
+  .then(user => {
     if (!user) {
-        return User.create({
-            name: 'Cornelia',
-            email: 'ekpenyong@gmail.com'
-        })
-       
+      return User.create({ name: 'Cornelia', email: 'test@test.com' });
     }
     return user;
-})
-.then( user => {
+  })
+  .then(user => {
+    // console.log(user);
     return user.createCart();
-    
-})
-.then( cart => {
+  })
+  .then(cart => {
     app.listen(3000);
-})
-.catch(err => console.log(err));
+  })
+  .catch(err => {
+    console.log(err);
+  });
