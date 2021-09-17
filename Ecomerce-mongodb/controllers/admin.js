cc87c8a17b2b7800ff4c757fa5a42ef9fa4ab46b
cornelia247 <ekpenyongcornelia2@gmail.com>
2021-09-17 22:25:11 +0100

    mongodb cart

--- Ecomerce-mongodb/controllers/admin.js
+++ Ecomerce-mongodb/controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 
+
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -13,18 +14,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-    userId: req.user.id
-  })
-  .then( result => {
-    console.log('Created Product')
-    res.redirect('/admin/products')
-  })
-  .catch(err => console.log(err));
+  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  product.save()
+  
+    .then( result => {
+      console.log('Created Product')
+      res.redirect('/admin/products')
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -33,10 +30,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-  .getProducts({ where: { id: prodId}})
-  .then(products => {
-    const product = products[0];
+  Product
+  .findById(prodId)
+  .then(product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -56,15 +52,11 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-  .then( product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.imageUrl = updatedImageUrl;
-    product.description = updatedDesc;
-    return product.save();
-  
-  })
+
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
+
+  product
+  .save()
   .then(result => {
     console.log('updated Product');
     res.redirect('/admin/products');
@@ -74,8 +66,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-  .getProducts()
+  Product
+  .fetchAll()
   .then( products => {
     res.render('admin/products', {
       prods: products,
@@ -83,16 +75,13 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     })
     })
-    .catch(err => console.log(err));
+  .catch(err => console.log(err));
  
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-  .then( product => {
-    return product.destroy();
-  })
+  Product.deleteById(prodId)
   .then( result => {
     console.log('PRODUCT DESTROYED')
     res.redirect('/admin/products')
--- Ecomerce-mongodb/util/database.js
+++ Ecomerce-mongodb/util/database.js
@@ -1,6 +1,29 @@
-const Sequelize = require('sequelize').Sequelize;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-ecomerce','root','cornelia247',{dialect: 'mysql',
-host: 'localhost'});
+ let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://cornelia247:DAS6ZdafNP5OZRKL@cluster0.jlem2.mongodb.net/shop?retryWrites=true&w=majority')
+.then( client => {
+    console.log('Succesfully Connnected to Mongodb Database!');
+    _db = client.db();
+    callback();
+})
+.catch( err => {
+    console.log(err);
+    throw err
+});
+};
+
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No database Found!';
+
+};
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
